308b460d53730165ce4b1f073df4e05984d1b7ad
Philipe Sena Saviott <psaviott@hotmail.com>
2021-09-30 14:47:45 -0300

    fix:  rest count

--- moneybillscounter.py
+++ moneybillscounter.py
@@ -1,28 +1,28 @@
 valor = int(input('Digite o valor em R$ '))
 
+
+if valor >= 100:
+    celulas = valor // 100
+    valor = valor - (celulas * 100)
+    print('{} cédulas de 100' .format(celulas))
+if valor >= 50:
+    celulas = valor // 50
+    valor = valor - (celulas * 50)
+    print('{} cédulas de 50' .format(celulas))
+if valor >= 20:
+    celulas = valor // 20
+    valor = valor - (celulas * 20)
+    print('{} cédulas de 20' .format(celulas))
+if valor >= 10:
+    celulas = valor // 10
+    valor = valor - (celulas * 10)
+    print('{} cédulas de 10' .format(celulas))
+if valor >= 5:
+    celulas = valor // 5
+    valor = valor - (celulas * 5)
+    print('{} cédulas de 5' .format(celulas))
 if valor:
-    if valor >= 100:
-        celulas = valor // 100
-        valor = valor - (celulas * 100)
-        print('{} cédulas de 100' .format(celulas))
-    if valor >= 50:
-        celulas = valor // 50
-        valor = valor - (celulas * 50)
-        print('{} cédulas de 50' .format(celulas))
-    if valor >= 20:
-        celulas = valor // 20
-        valor = valor - (celulas * 20)
-        print('{} cédulas de 20' .format(celulas))
-    if valor >= 10:
-        celulas = valor // 10
-        valor = valor - (celulas * 10)
-        print('{} cédulas de 10' .format(celulas))
-    if valor >= 5:
-        celulas = valor // 5
-        valor = valor - (celulas * 5)
-        print('{} cédulas de 5' .format(celulas))
-    if 0 < valor < 5:
-        celulas = valor
-        print('{} moedas de 1 que pobreza hein mana' .format(celulas))
+    celulas = valor
+    print('{} moedas de 1 que pobreza hein mana' .format(celulas))
 
 # top
